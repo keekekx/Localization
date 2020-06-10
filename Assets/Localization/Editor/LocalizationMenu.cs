6798dc763390840326de9c46a1740c48d17a72eb
kexiang <kexiang@thinkplay.co>
2020-06-10 15:34:23 +0800

    init

---
+++ Assets/Localization/Editor/LocalizationMenu.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.UI;
+
+public static class LocalizationMenu
+{
+    [MenuItem("GameObject/UI/LocalizationText", false, 30)]
+    static void CreateLocalizationTextGameObject(MenuCommand menuCommand)
+    {
+        var go = new GameObject("LocalizationText");
+        var rt = go.AddComponent<RectTransform>();
+        var txt = go.AddComponent<LocalizationText>();
+        var parent = menuCommand.context as GameObject;
+        if (parent == null)
+        {
+            parent = Object.FindObjectOfType(typeof(Canvas)) as GameObject;
+            if (parent == null)
+            {
+                var root = new GameObject("Canvas");
+                root.layer = LayerMask.NameToLayer("UI");
+                Canvas canvas = root.AddComponent<Canvas>();
+                canvas.renderMode = RenderMode.ScreenSpaceOverlay;
+                root.AddComponent<CanvasScaler>();
+                root.AddComponent<GraphicRaycaster>();
+                Undo.RegisterCreatedObjectUndo(root, "Create " + root.name);
+                parent = root;
+            }
+        }
+        rt.SetParent(parent.transform);
+        rt.localPosition = Vector3.zero;
+        rt.localScale = Vector3.one;
+        txt.SetText("Hello World");
+        Undo.RegisterCreatedObjectUndo(go, "Create " + go.name);
+        Selection.activeGameObject = go;
+    }
+
+    [MenuItem("GameObject/UI/LocalizationImage", false, 31)]
+    static void CreateLocalizationImageGameObject(MenuCommand menuCommand)
+    {
+        var go = new GameObject("LocalizationImage");
+        var rt = go.AddComponent<RectTransform>();
+        var image = go.AddComponent<LocalizationImage>();
+        var parent = menuCommand.context as GameObject;
+        if (parent == null)
+        {
+            parent = Object.FindObjectOfType(typeof(Canvas)) as GameObject;
+            if (parent == null)
+            {
+                var root = new GameObject("Canvas");
+                root.layer = LayerMask.NameToLayer("UI");
+                Canvas canvas = root.AddComponent<Canvas>();
+                canvas.renderMode = RenderMode.ScreenSpaceOverlay;
+                root.AddComponent<CanvasScaler>();
+                root.AddComponent<GraphicRaycaster>();
+                Undo.RegisterCreatedObjectUndo(root, "Create " + root.name);
+                parent = root;
+            }
+        }
+        rt.SetParent(parent.transform);
+        rt.localPosition = Vector3.zero;
+        rt.localScale = Vector3.one;
+        Undo.RegisterCreatedObjectUndo(go, "Create " + go.name);
+        Selection.activeGameObject = go;
+    }
+}
---
+++ Assets/Localization/Scripts/LocalizationText.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+[AddComponentMenu("UI/LocalizationText")]
+public class LocalizationText : TMPro.TextMeshProUGUI, IOnLocalizationRegionChange
+{
+    public bool UseLocalization;
+    public string LocalizationKey;
+    public string LocalizationTag = "default";
+
+    protected override void Awake()
+    {
+        base.Awake();
+        LocalizationSystem.RegisterComponent(this);
+
+        if (UseLocalization)
+        {
+            SetText(LocalizationKey);
+        }
+    }
+
+    public new void SetText(string key)
+    {
+        SetText(key, LocalizationTag);
+    }
+
+    public void SetText(string key, string tag = "default")
+    {
+        if (UseLocalization)
+        {
+            if (string.IsNullOrEmpty(key))
+            {
+                return;
+            }
+
+            LocalizationKey = key;
+
+            if (!string.IsNullOrEmpty(tag))
+            {
+                LocalizationTag = tag;
+            }
+
+            key = LocalizationSystem.GetText(LocalizationKey, LocalizationTag);
+        }
+
+        key = LocalizationSystem.ReplaceMarco(key);
+        base.SetText(key);
+    }
+
+    public void OnLocalizationRegionChange()
+    {
+        if (UseLocalization)
+        {
+            SetText(LocalizationKey);
+        }
+    }
+
+    protected override void OnDestroy()
+    {
+        base.OnDestroy();
+        LocalizationSystem.UnRegisterComponent(this);
+    }
+}
